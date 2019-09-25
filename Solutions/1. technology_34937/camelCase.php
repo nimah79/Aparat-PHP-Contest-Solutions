21559ea53b009239e761b3bc6de65989c42200fb
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-09-25 18:47:35 +0330

    Add solutions

---
+++ Solutions/1. technology_34937/camelCase.php
@@ -0,0 +1,3 @@
+<?php
+
+echo ucwords(trim(str_replace(' ', '', file_get_contents(__DIR__ . '/input.txt'))));
---
+++ Solutions/2. technology_34938/SHA1Hash.php
@@ -0,0 +1,29 @@
+<?php
+
+class SHA1Hash implements HashStrategy
+{
+
+    private static $instance = null;
+
+    protected function __construct() { }
+    protected function __clone() { }
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+
+    public function hash($data)
+    {
+        return sha1($data);
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null)
+        {
+          self::$instance = new SHA1Hash();
+        }
+        
+        return self::$instance;
+    }
+}
